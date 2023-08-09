96473938808617013c53be8b3951a4dbdcfdc513
valmon55 <fedor_ka@mail.ru>
2023-08-09 17:22:43 +0300

    +class

--- CS_Basic/CS_Basic.Task_1/Program.cs
+++ CS_Basic/CS_Basic.Task_1/Program.cs
@@ -6,5 +6,20 @@
         {
             Console.WriteLine("Hello, World!");
         }
+        static void ShowAds()
+        {
+            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+            // Остановка на 1 с
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+            // Остановка на 2 с
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+            // Остановка на 3 с
+            Thread.Sleep(3000);
+        }
     }
+
 }
---
+++ CS_Basic/CS_Basic.Task_1/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Basic.Task_1
+{
+    internal class User
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public bool IsPremium { get; set; }
+    }
+}
